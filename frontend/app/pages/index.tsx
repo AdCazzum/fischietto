6df24f85222739f990e4eadfb6b93b4517789ece
Ivan Sala <slavni96@gmail.com>
2024-10-05 11:36:04 +0200

    What are we doing here

--- frontend/app/pages/index.tsx
+++ frontend/app/pages/index.tsx
@@ -13,14 +13,12 @@ export default function Home() {
 
       <div className="page">
         <main className="intro">
-          <h1 className="intro-title">Welcome to StealthNote</h1>
+          <h1 className="intro-title">Welcome to Fischetto 🤫🤐👮🔗</h1>
           <p>
-            StealthNote is an application for people in an organization to
-            anonymously broadcast messages.
+            Fischietto is a secure whistleblowing platform that enables you to anonymously report workplace issues while ensuring your identity is protected.
           </p>
           <p>
-            We use Zero Knowledge Proofs to prove that you are part of an
-            organization without revealing any information about yourself.
+            We use <a target="_blank" href="https://aztec.network/">Aztec</a> Zero Knowledge Proofs (ZKPs) to verify that you belong to your organization without revealing personal information, and <a target="_blank" href="https://www.fhenix.io/">Fhenix</a> for encrypted operations on reports and votes, ensuring end-to-end confidentiality through Fully Homomorphic Encryption (FHE).
           </p>
           <p>
             Sign in with your <u>work Google account</u> (<span className="inline-code">you@company.com</span>) to
